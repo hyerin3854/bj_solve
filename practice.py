3fccc38e4171fe8181331111a84cee8605cfe3dc
hyerin ma <hyerin3854@gmail.com>
2021-08-04 17:49:51 +0900

    pygame 만들기

---
+++ practice.py
@@ -0,0 +1,8 @@
+hyerin = "990502-1234567"
+
+print("성별 : " + hyerin[7])
+print("연도 : " + hyerin[0:2])
+print("월 : " + hyerin[2:4])
+print("일 : " + hyerin[4:6])
+print("생년월일 : " + hyerin[:6])
+print("뒤 7자리 : " + hyerin[-7:])
